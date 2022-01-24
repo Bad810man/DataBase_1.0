a4830e004f7b22ad93aecd6911c75e3cf8b6bcbd
Valery Kabanov <kabanovv310@gmail.com>
2022-01-24 15:49:02 +0300

    Adding methods for viewing records

--- src/main/java/org/DataBaseClass.java
+++ src/main/java/org/DataBaseClass.java
@@ -9,7 +9,7 @@ public class DataBaseClass {
     static final String USER = "root";
     static final String PASSWORD = "decay13351";
 
-    Connection connection;
+    protected static Connection connection;
 
     DataBaseClass () {
         try {
@@ -22,7 +22,51 @@ public class DataBaseClass {
     }
 
     // Method for close connection.
-    protected void CloseConnection() throws SQLException{
+    protected void closeConnection() throws SQLException{
         connection.close();
+    }
+
+    // Method for show all records in database
+    protected void showAllRecords() {
+        String showAll = "SELECT * FROM Students";
+        showInfo(showAll);
+    }
+
+    // Method for show info about certain group
+    protected void showCertainGroup(String groupN) {
+        String certainGroup = "SELECT * FROM Students WHERE Группа = '" + groupN + "';";
+        showInfo(certainGroup);
+    }
+
+    // Method for show info about certain student
+    protected void showCertainStudent(String student) {
+        String certainStudent = "SELECT * FROM Students WHERE Студенческий билет = '" + student + "';";
+        showInfo(certainStudent);
+    }
+
+    private static void showInfo(String sql) {
+        try (Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                String lastName = rs.getString("Фамилия");
+                String firstName = rs.getString("Имя");
+                String middleName = rs.getString("Отчество");
+                Date birthDate = rs.getDate("Дата рождения");
+                String group = rs.getString("Группа");
+                String studentBook = rs.getString("Студенческий билет");
+                String recordBook = rs.getString("Зачетная книжка");
+                System.out.println("\n==============");
+                System.out.println("Фамилия: " + lastName);
+                System.out.println("Имя: " + firstName);
+                System.out.println("Отчество: " + middleName);
+                System.out.println("Дата рождения: " + birthDate);
+                System.out.println("Группа: " + group);
+                System.out.println("Студенческий билет №: " + studentBook);
+                System.out.println("Зачетная книжка №: " + recordBook);
+                System.out.println("==============\n");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/org/TestDB.java
+++ src/test/java/org/TestDB.java
@@ -5,32 +5,34 @@ import java.util.Scanner;
 
 public class TestDB {
     public static void main(String[] args) {
-        DataBaseClass connect = new DataBaseClass();
+        DataBaseClass dataBase = new DataBaseClass();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите номер пукта меню:");
-        System.out.println("1. Добавить запись");
-        System.out.println("2. Добавить запись");
-        System.out.println("3. Вывести данные о студенте");
+        System.out.println("1. Посмотреть все записи");
+        System.out.println("2. Посмотреть записи конкретной группы");
+        System.out.println("3. Посмотреть информацию о конкретном студенте");
         System.out.println("0. Выход");
 
         String in = scanner.nextLine();
         switch (in) {
             case "1":
-                System.out.println("1");
+                dataBase.showAllRecords();
                 break;
             case "2":
-                System.out.println("2");
+                System.out.println("Введите номер группы:");
+                String gr = scanner.nextLine();
+                dataBase.showCertainGroup(gr);
                 break;
             case "3":
                 System.out.println("Введите номер студенческого билета:");
                 String studentBook = scanner.nextLine();
-                // searchStudent(studentBook);
+                dataBase.showCertainStudent(studentBook);
                 break;
             case "0":
                 try {
-                    connect.CloseConnection();
+                    dataBase.closeConnection();
                 } catch (SQLException e) {
                     e.printStackTrace();
                 }
